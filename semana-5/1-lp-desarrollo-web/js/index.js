63d227e68bd1d0a82aefc4f5c1f92eb326198659
ElliotXLeo <elliotgaramendi@gmail.com>
2022-02-24 23:01:28 -0500

    s5.1: header

---
+++ semana-5/1-lp-desarrollo-web/js/index.js
@@ -0,0 +1,32 @@
+'use strict';
+const documentReady = () => {
+
+  const header = document.querySelector('.header');
+  const headerNavMenuIconContainer = document.getElementById('headerNavMenuIconContainer');
+  const headerNavMenuIcon = document.querySelector('.header-nav__menu-icon');
+  const headerNavCloseIconContainer = document.getElementById('headerNavCloseIconContainer');
+  const headerNavLinkList = document.querySelector('.header-nav__link-list');
+  const headerNavLinks = [...document.querySelectorAll('.header-nav__link')];
+
+  const documentScroll = () => {
+    header.classList.toggle('header--scroll', window.scrollY > 0);
+    headerNavMenuIcon.classList.toggle('header-nav__menu-icon--scroll', window.scrollY > 0);
+    headerNavLinks.map((element) => {
+      element.classList.toggle('header-nav__link--scroll', window.scrollY > 0);
+    });
+  };
+
+  const openMenu = () => {
+    headerNavLinkList.classList.add('header-nav__link-list--open');
+  };
+
+  const closeMenu = () => {
+    headerNavLinkList.classList.remove('header-nav__link-list--open');
+  };
+
+  document.addEventListener('scroll', documentScroll);
+  headerNavMenuIconContainer.addEventListener('click', openMenu);
+  headerNavCloseIconContainer.addEventListener('click', closeMenu);
+
+}
+document.addEventListener('DOMContentLoaded', documentReady);
