162a39e1b66aed0ba0edfac0da4e18b311d186f3
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-03 20:15:06 -0500

    s5.2: M. DOM con data interna perfil

--- semana-5/2-pagina-personal/js/index.js
+++ semana-5/2-pagina-personal/js/index.js
@@ -1,6 +1,18 @@
 'use strict';
 const documentReady = () => {
+
+  const perfil = [
+    'Elliot Garamendi',
+    'Elliot Garamendi Desarrollador de tus sueños',
+    'Estudiante de Ingeniería de Software perteneciente a los primeros puestos. Apasionado del autoaprendizaje con el sueño de crecer en el Desarrollo Web Frontend con la biblioteca React. Asimismo, amo compartir los conocimientos adquiridos día a día mediante la docencia.',
+    'https://1drv.ms/u/s!As5U-bd3F-a6gYsmbbG-Dybk88biRQ?e=Rifl4V',
+    'https://i.postimg.cc/bvJLLgZd/perfil-elliotgaramendi.jpg'
+  ];
+
   // Start header
+  const headerNavMenu = document.getElementById('headerNavMenu');
+  const headerNavLinkItems = [...document.querySelectorAll('.header-nav__link-item')];
+
   const headerScroll = () => {
     const header = document.getElementById('header');
     const headerNavMenuIcon = document.querySelector('.header-nav__menu-icon');
@@ -12,20 +24,34 @@ const documentReady = () => {
       element.classList.toggle('header-nav__link--scroll', window.scrollY > 0);
     });
   };
-  window.addEventListener('scroll', headerScroll);
 
   const toggleMenu = () => {
     const headerNavLinkList = document.getElementById('headerNavLinkList');
     headerNavLinkList.classList.toggle('header-nav__link-list--left-0');
   };
-  const headerNavMenu = document.getElementById('headerNavMenu');
-  headerNavMenu.addEventListener('click', toggleMenu);
 
-  const headerNavLinkItems = [...document.querySelectorAll('.header-nav__link-item')];
+  window.addEventListener('scroll', headerScroll);
+  headerNavMenu.addEventListener('click', toggleMenu);
   headerNavLinkItems.map((element) => {
     element.addEventListener('click', toggleMenu);
   });
   // End header
+
+  // Start perfil
+  const getInfoPerfil = () => {
+    const perfilInfoTitle = document.getElementById('perfilInfoTitle');
+    const perfilInfoDescription = document.getElementById('perfilInfoDescription');
+    const perfilInfoLinkCv = document.getElementById('perfilInfoLinkCv');
+    const perfilImage = document.getElementById('perfilImage');
+
+    perfilInfoTitle.innerHTML = perfil[1];
+    perfilInfoDescription.innerHTML = perfil[2];
+    perfilInfoLinkCv.href = perfil[3];
+    perfilImage.src = perfil[4];
+    perfilImage.alt = perfil[0];
+  }
+  getInfoPerfil();
+  // End perfil
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
