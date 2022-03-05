ac46f132f89f19c4c0f034028c28df28b641bbfb
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-04 20:06:01 -0500

    s5.2: M. DOM con data interna habilidades

--- semana-5/2-pagina-personal/js/index.js
+++ semana-5/2-pagina-personal/js/index.js
@@ -488,7 +488,290 @@ const documentReady = () => {
     // }
   ];
 
-  // Start header
+  const habilidades = [
+    {
+      "id": 0,
+      "name": "React",
+      "core": "Frontend",
+      "percentage": 50,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 1,
+      "name": "Vue",
+      "core": "Frontend",
+      "percentage": 20,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 2,
+      "name": "Angular",
+      "core": "Frontend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 3,
+      "name": "JavaScript",
+      "core": "Frontend",
+      "percentage": 80,
+      "level": "Avanzado",
+      "main": 1
+    },
+    {
+      "id": 4,
+      "name": "CSS",
+      "core": "Frontend",
+      "percentage": 80,
+      "level": "Avanzado",
+      "main": 1
+    },
+    {
+      "id": 5,
+      "name": "HTML",
+      "core": "Frontend",
+      "percentage": 80,
+      "level": "Avanzado",
+      "main": 1
+    },
+    {
+      "id": 6,
+      "name": "TypeScript",
+      "core": "Frontend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 1
+    },
+    {
+      "id": 7,
+      "name": "Sass",
+      "core": "Frontend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 1
+    },
+    {
+      "id": 8,
+      "name": "Bootstrap",
+      "core": "Frontend",
+      "percentage": 60,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 9,
+      "name": "jQuery",
+      "core": "Frontend",
+      "percentage": 40,
+      "level": "Intermedio",
+      "main": 0
+    },
+    {
+      "id": 10,
+      "name": "RWD",
+      "core": "Frontend",
+      "percentage": 60,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 11,
+      "name": "BEM",
+      "core": "Frontend",
+      "percentage": 75,
+      "level": "Avanzado",
+      "main": 1
+    },
+    {
+      "id": 12,
+      "name": "UX/UI",
+      "core": "Frontend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 1
+    },
+    {
+      "id": 13,
+      "name": "SEO",
+      "core": "Frontend",
+      "percentage": 40,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 14,
+      "name": "Node.js",
+      "core": "Backend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 1
+    },
+    {
+      "id": 15,
+      "name": "Express.js",
+      "core": "Backend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 1
+    },
+    {
+      "id": 16,
+      "name": "Next.js",
+      "core": "Backend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 17,
+      "name": "Java",
+      "core": "Backend",
+      "percentage": 50,
+      "level": "Intermedio",
+      "main": 0
+    },
+    {
+      "id": 18,
+      "name": "Spring Boot",
+      "core": "Backend",
+      "percentage": 20,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 19,
+      "name": "PHP",
+      "core": "Backend",
+      "percentage": 50,
+      "level": "Intermedio",
+      "main": 0
+    },
+    {
+      "id": 20,
+      "name": "Laravel",
+      "core": "Backend",
+      "percentage": 20,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 21,
+      "name": "API",
+      "core": "Backend",
+      "percentage": 40,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 22,
+      "name": "REST",
+      "core": "Backend",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 23,
+      "name": "MySQL",
+      "core": "Base de datos",
+      "percentage": 60,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 24,
+      "name": "Microsoft SQL Server",
+      "core": "Base de datos",
+      "percentage": 50,
+      "level": "Intermedio",
+      "main": 0
+    },
+    {
+      "id": 25,
+      "name": "PostgreSQL",
+      "core": "Base de datos",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 26,
+      "name": "Oracle DB",
+      "core": "Base de datos",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 27,
+      "name": "MongoDB",
+      "core": "Base de datos",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 1
+    },
+    {
+      "id": 28,
+      "name": "git",
+      "core": "Tecnologías",
+      "percentage": 60,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 29,
+      "name": "Calidad de software",
+      "core": "Tecnologías",
+      "percentage": 40,
+      "level": "Intermedio",
+      "main": 1
+    },
+    {
+      "id": 30,
+      "name": "Jest",
+      "core": "Fullstack",
+      "percentage": 20,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 31,
+      "name": "Automatización",
+      "core": "Tecnologías",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 32,
+      "name": "Linux",
+      "core": "Tecnologías",
+      "percentage": 40,
+      "level": "Intermedio",
+      "main": 0
+    },
+    {
+      "id": 33,
+      "name": "Amazon Web Services",
+      "core": "Tecnologías",
+      "percentage": 30,
+      "level": "Básico",
+      "main": 0
+    },
+    {
+      "id": 34,
+      "name": "Scrum",
+      "core": "Complementario",
+      "percentage": 60,
+      "level": "Intermedio",
+      "main": 1
+    }
+  ];
+
+  // inicio header
   const headerNavMenu = document.getElementById('headerNavMenu');
   const headerNavLinkItems = [...document.querySelectorAll('.header-nav__link-item')];
 
@@ -514,9 +797,9 @@ const documentReady = () => {
   headerNavLinkItems.map((element) => {
     element.addEventListener('click', toggleMenu);
   });
-  // End header
+  // fin header
 
-  // Start perfil
+  // inicio perfil
   const getInfoPerfil = () => {
     const perfilInfoTitle = document.getElementById('perfilInfoTitle');
     const perfilInfoDescription = document.getElementById('perfilInfoDescription');
@@ -530,10 +813,10 @@ const documentReady = () => {
     perfilImage.alt = perfil[0];
   }
   getInfoPerfil();
-  // End perfil
+  // fin perfil
 
-  // Start experiencia
-  const getExperiencias = () => {
+  // inicio experiencia
+  const getExperiencias = (experiencias) => {
     const experienciaExperiencias = document.getElementById('experienciaExperiencias');
     [...experiencias].reverse().forEach((element) => {
       if (element.main === 1) {
@@ -551,18 +834,18 @@ const documentReady = () => {
       }
     });
   };
-  getExperiencias();
-  // End experiencia
+  getExperiencias(experiencias);
 
-  // Start proyectos
-  const getProyectos = () => {
+  // fin experiencia
+
+  // inicio proyectos
+  const getProyectos = (datos) => {
     const proyectosProyectos = document.getElementById('proyectosProyectos');
-    const proyectosPrincipales = proyectos.filter((element) => {
+    const proyectosPrincipales = datos.filter((element) => {
       return (element.main === 1);
     });
     proyectosPrincipales.reverse().forEach((element) => {
-      if (element.main === 1) {
-        proyectosProyectos.innerHTML += `
+      proyectosProyectos.innerHTML += `
         <article class="projects__project">
           <figure class="projects__project-image-container">
             <img src="${element.image}" alt="${element.title}" class="projects__project-image" />
@@ -579,11 +862,30 @@ const documentReady = () => {
           </div>
         </article>
       `;
-      }
     });
   };
-  getProyectos();
-  // End proyectos
+  getProyectos(proyectos);
+  // fin proyectos
+
+  // inicio habilidades
+  const habilidadesHabilidadesList = document.getElementById('habilidadesHabilidadesList');
+
+  const getHtmlHabilidades = (datos) => {
+    const datosPrincipales = datos.filter((element) => {
+      return (element.main === 1);
+    });
+    let html = '';
+    datosPrincipales.forEach((element) => {
+      html += `
+        <li class="habilidades__habilidad">${element.name}</li>
+      `;
+    });
+    return html;
+  };
+  
+  let habilidadesHtml = getHtmlHabilidades(habilidades);
+  habilidadesHabilidadesList.innerHTML = habilidadesHtml;
+  // fin habilidades
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
