01700533fa37cf2f8024b8ac104b580c7d129500
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-04 23:05:22 -0500

    s5.2: Data interna en archivos JS

---
+++ semana-5/2-pagina-personal/js/perfil.js
@@ -0,0 +1,8 @@
+const perfil = [
+  'Elliot Garamendi',
+  'Elliot Garamendi Desarrollador de tus sueños',
+  'Estudiante de Ingeniería de Software perteneciente a los primeros puestos. Apasionado del autoaprendizaje con el sueño de crecer en el Desarrollo Web Frontend con la biblioteca React. Asimismo, amo compartir los conocimientos adquiridos día a día mediante la docencia.',
+  'https://1drv.ms/u/s!As5U-bd3F-a6gYsmbbG-Dybk88biRQ?e=Rifl4V',
+  'https://i.postimg.cc/bvJLLgZd/perfil-elliotgaramendi.jpg',
+  'https://www.linkedin.com/in/elliotgaramendi/'
+];
