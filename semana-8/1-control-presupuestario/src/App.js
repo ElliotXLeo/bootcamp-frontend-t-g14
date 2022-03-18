71ead1361aeeda22dc3659aff0ba4595e546d1f2
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-17 21:03:37 -0500

    s8.1: Instalación de SweetAlert2

--- semana-8/1-control-presupuestario/src/App.js
+++ semana-8/1-control-presupuestario/src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
+import Swal from 'sweetalert2';
 
 function App() {
+
+  Swal.fire({
+    title: 'Control Presupuestario',
+    icon: 'success',
+    title: 'Instalación correcta',
+    text: 'Bienvenidos a Control Presupuestario',
+    footer: '<a href="#">Conocer más</a>',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
