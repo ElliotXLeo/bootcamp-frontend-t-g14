80c78d5b9aa64546a89e7a730fab873b56e2b607
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-17 20:54:16 -0500

    s8.1: Instalación de Animete.css

--- semana-8/1-control-presupuestario/src/App.js
+++ semana-8/1-control-presupuestario/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo animate__animated animate__fadeIn" alt="logo" />
         <p>
           Control Presupuestario
         </p>
