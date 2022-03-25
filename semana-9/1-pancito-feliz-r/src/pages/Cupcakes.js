70df886f377754e4ea153f49bfe6bc8f11f19875
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-24 21:30:04 -0500

    s9.1: consumo de data

--- semana-9/1-pancito-feliz-r/src/pages/Cupcakes.js
+++ semana-9/1-pancito-feliz-r/src/pages/Cupcakes.js
@@ -1,13 +1,31 @@
+import { useState, useEffect } from 'react';
 import '../styles/css/Cupcakes.css';
 
 const Cupcakes = () => {
+
+  const [cupcakes, setCupcakes] = useState([]);
+
+  useEffect(() => {
+    const apiCupcakes = async () => {
+      try {
+        const api = await fetch('http://localhost:5500/cupcakes');
+        const data = await api.json();
+        setCupcakes(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        console.log('Se consultó a la API');
+      }
+    };
+    apiCupcakes();
+  }, []);
 
   return (
     <section className="cupcakes cupcakes--pt-4">
       <div className="cupcakes-container">
         <h1 className="cupcakes__title">🧁 Cupcakes 🧁</h1>
         {
-          false
+          cupcakes.length === 0
             ?
             (
               <section className="spinner">
@@ -20,26 +38,30 @@ const Cupcakes = () => {
             :
             (
               <div className="cupcakes__cupcakes">
-                <article className="cupcakes__cupcake">
-                  <figure className="cupcakes__cupcake-image-container">
-                    <img
-                      src="https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/04/como-hacer-cupcakes-de-vainilla-para-vender.jpg"
-                      alt="Foto de cupcake"
-                      className="cupcakes__cupcake-image-container" />
-                  </figure>
+                {cupcakes.map(({ id, sabor, color, descripcion, precio, imagen }) => {
+                  return (
+                    <article className="cupcakes__cupcake" key={id}>
+                      <figure className="cupcakes__cupcake-image-container">
+                        <img
+                          src={imagen}
+                          alt={descripcion}
+                          className="cupcakes__cupcake-image-container" />
+                      </figure>
 
-                  <div className="cupcakes__cupcake-data">
-                    <h2 className="cupcakes__cupcake-data-sabor">Sabor: Vainilla</h2>
-                    <h3 className="cupcakes__cupcake-data-precio">Precio: 10</h3>
-                    <h4 className="cupcakes__cupcake-data-color">Color: amarillo</h4>
-                    <p className="cupcakes__cupcake-data-about">1: Gran cupcake sabor vainilla clásica</p>
-                  </div>
+                      <div className="cupcakes__cupcake-data">
+                        <h2 className="cupcakes__cupcake-data-sabor">Sabor: {sabor}</h2>
+                        <h3 className="cupcakes__cupcake-data-precio">Precio: {precio}</h3>
+                        <h4 className="cupcakes__cupcake-data-color">Color: {color}</h4>
+                        <p className="cupcakes__cupcake-data-about">{id}: {descripcion}</p>
+                      </div>
 
-                  <div className="cupcakes__cupcake-button-container">
-                    <a className="cupcakes__cupcake-button cupcakes__cupcake-button--active">Ver</a>
-                    <a className="cupcakes__cupcake-button">Comprar</a>
-                  </div>
-                </article>
+                      <div className="cupcakes__cupcake-button-container">
+                        <button className="cupcakes__cupcake-button cupcakes__cupcake-button--active">Ver</button>
+                        <button className="cupcakes__cupcake-button">Comprar</button>
+                      </div>
+                    </article>
+                  );
+                })}
               </div>
             )
         }
