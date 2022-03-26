de26dffa8ab500c99db9eb5fcf59993c4cccb1a3
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-25 22:26:10 -0500

    s9.1: Instalación y configuración de sweetalert2

--- semana-9/1-pancito-feliz-r/src/components/cards/CardCupcakes.js
+++ semana-9/1-pancito-feliz-r/src/components/cards/CardCupcakes.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import useApi from '../../hooks/useApi';
 import Spinner from '../Spinner';
 import CardCupcake from './CardCupcake';
@@ -9,35 +10,48 @@ const CardCupcakes = ({ title, filter }) => {
   const [cupcakes, error] = useApi(recurso);
 
   if (error) {
-    return <h2>Error</h2>
-  } else {
-    return (
-      <section className={`cupcakes ${filter ? '' : 'cupcakes--pt-4'} animate__animated animate__fadeIn`}>
-        <div className="cupcakes-container">
-          <h1 className="cupcakes__title">{title}</h1>
-          {
-            cupcakes.length === 0
-              ?
-              (
-                <Spinner />
-              )
-              :
-              (
-                <div className="cupcakes__cupcakes">
-                  {cupcakes.map((cupcake) => {
-                    return (
-                      <CardCupcake
-                        key={cupcake.id}
-                        cupcake={cupcake}
-                      />
-                    );
-                  })}
-                </div>
-              )
-          }
-        </div>
-      </section>
-    );
+    Swal.fire({
+      position: 'center',
+      title: '¡Ha ocurrido un error! Contactar con soporte.',
+      text: error,
+      icon: 'error',
+      confirmButtonText: 'Aceptar',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    });
   }
+
+  return (
+    <section className={`cupcakes ${filter ? '' : 'cupcakes--pt-4'} animate__animated animate__fadeIn`}>
+      <div className="cupcakes-container">
+        <h1 className="cupcakes__title">{title}</h1>
+        {
+          cupcakes.length === 0
+            ?
+            (
+              <Spinner />
+            )
+            :
+            (
+              <div className="cupcakes__cupcakes">
+                {cupcakes.map((cupcake) => {
+                  return (
+                    <CardCupcake
+                      key={cupcake.id}
+                      cupcake={cupcake}
+                    />
+                  );
+                })}
+              </div>
+            )
+        }
+      </div>
+    </section>
+  );
 }
+
 export default CardCupcakes;
