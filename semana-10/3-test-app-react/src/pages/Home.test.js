d0438cdf7935330bf4bfc6bacf479557173a1761
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-02 10:44:19 -0500

    Refactor Test Home

--- semana-10/3-test-app-react/src/pages/Home.test.js
+++ semana-10/3-test-app-react/src/pages/Home.test.js
@@ -1,13 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Home from "./Home";
 
+beforeEach(() => {
+  render(<Home />);
+});
+
 describe('Home', () => {
   it('Must reder', () => {
-    render(<Home />);
   });
 
   it('Must display a title', () => {
-    render(<Home />);
     expect(screen.queryByText(/home title/i)).toBeInTheDocument();
   });
 });
