b388f617e9622bd56e91fb38e915664aa58d00a8
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-01 20:20:10 -0500

    class component a functional component

--- semana-10/2-game-store-redux-react/src/components/pokemons/PokemonActions.js
+++ semana-10/2-game-store-redux-react/src/components/pokemons/PokemonActions.js
@@ -1,37 +1,32 @@
-import { Component } from "react";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { gamesPokemonBuyAction, gamesPokemonReturnAction } from "../../redux/actions/gameStoreActions";
 
-class PokemonActions extends Component {
-  render() {
-    return (
-      <div className="btn-group mb-3" role="group" aria-label="Basic example">
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={() => {
-            { this.props.gamesPokemonBuyAction(1) };
-          }}
-        >
-          Comprar
-        </button>
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={() => {
-            { this.props.gamesPokemonReturnAction(1) };
-          }}
-        >
-          Retornar
-        </button>
-      </div>
-    );
-  }
+const PokemonActions = () => {
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="btn-group mb-3" role="group" aria-label="Basic example">
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          dispatch(gamesPokemonBuyAction(1));
+        }}
+      >
+        Comprar
+      </button>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        onClick={() => {
+          dispatch(gamesPokemonReturnAction(1));
+        }}
+      >
+        Retornar
+      </button>
+    </div>
+  );
 }
 
-const mapDispatchToProps = {
-  gamesPokemonBuyAction,
-  gamesPokemonReturnAction
-};
-
-export default connect(null, mapDispatchToProps)(PokemonActions);
+export default PokemonActions;
--- semana-10/2-game-store-redux-react/src/components/pokemons/PokemonStock.js
+++ semana-10/2-game-store-redux-react/src/components/pokemons/PokemonStock.js
@@ -1,18 +1,14 @@
-import { Component } from "react";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
-class PokemonStock extends Component {
-  render() {
-    return (
-      <p className="card-text">Stock: {this.props.gamesReducer.pokemon}</p>
-    );
-  }
+const PokemonStock = () => {
+
+  const gamesReducer = useSelector((state) => {
+    return (state.gamesReducer);
+  });
+
+  return (
+    <p className="card-text">Stock: {gamesReducer.pokemon}</p>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    gamesReducer: state.gamesReducer
-  }
-};
-
-export default connect(mapStateToProps)(PokemonStock);
+export default PokemonStock;
