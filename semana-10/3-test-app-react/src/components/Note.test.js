8c7931af4efc3d308abccf39054d85d243d82c97
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-02 11:54:50 -0500

    Update component Note Test

--- semana-10/3-test-app-react/src/components/Note.test.js
+++ semana-10/3-test-app-react/src/components/Note.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { prettyDOM, render } from "@testing-library/react";
 import Note from './Note';
 
 test('Render component Note', () => {
@@ -10,4 +10,22 @@ test('Render component Note', () => {
   render(<Note
     note={note}
   />);
+});
+
+test('Display title and Make not important', () => {
+  const note = {
+    content: 'This is a test',
+    important: true
+  };
+
+  const component = render(<Note
+    note={note}
+  />);
+
+  component.getByText(note.content);
+  component.getByText('Make not important');
+  // expect(component.container).toHaveTextContent(note.content);
+
+  const liElement = component.container.querySelector('li');
+  console.log(prettyDOM(liElement));
 });
