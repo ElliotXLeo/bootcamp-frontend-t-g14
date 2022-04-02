0c382e33e88dac5a321a46a0b58e093f39196ae5
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-02 11:40:56 -0500

    Refactor component Note

--- semana-10/3-test-app-react/src/components/Note.js
+++ semana-10/3-test-app-react/src/components/Note.js
@@ -1,6 +1,12 @@
-const Note = () => {
+const Note = ({ note, toggleImportance }) => {
+
+  const textButton = note.important ? 'Make not important' : 'Make important'
+
   return (
-    <></>
+    <li>
+      <h2>{note.content}</h2>
+      <button oncClick={toggleImportance}>{textButton}</button>
+    </li>
   );
 }
 
