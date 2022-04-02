5c276c645c943f52265f5adb09217fb4fe1761bc
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-01 19:29:59 -0500

    s10.2: redux mapDispatchToProps en PokemonActions

--- semana-10/2-game-store-redux-react/src/components/pokemons/PokemonActions.js
+++ semana-10/2-game-store-redux-react/src/components/pokemons/PokemonActions.js
@@ -1,10 +1,34 @@
-const PokemonActions = () => {
+import { connect } from "react-redux";
+import { gamesPokemonBuyAction, gamesPokemonReturnAction } from "../../redux/actions/gameStoreActions";
+
+const PokemonActions = ({ gamesPokemonBuyAction, gamesPokemonReturnAction }) => {
   return (
     <div className="btn-group mb-3" role="group" aria-label="Basic example">
-      <button type="button" className="btn btn-primary">Comprar</button>
-      <button type="button" className="btn btn-secondary">Retornar</button>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          gamesPokemonBuyAction(1);
+        }}
+      >
+        Comprar
+      </button>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        onClick={() => {
+          gamesPokemonReturnAction(1);
+        }}
+      >
+        Retornar
+      </button>
     </div>
   );
 }
 
-export default PokemonActions;
+const mapDispatchToProps = {
+  gamesPokemonBuyAction,
+  gamesPokemonReturnAction
+};
+
+export default connect(null, mapDispatchToProps)(PokemonActions);
