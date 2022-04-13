00f33b6a61e74d65217f16f059f6f838fc0ba014
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-13 14:46:22 -0500

    s7.2: Update consumo de data con axios desde endpoint

--- semana-7/2-pagina-personal/js/index.js
+++ semana-7/2-pagina-personal/js/index.js
@@ -5,15 +5,32 @@ import experiences from './experiences.js';
 import projects from './projects.js';
 import skills from './skills.js';
 import footer from './footer.js';
-import portafolio from './portafolio-elliot.js';
 
 const documentReady = () => {
-  header();
-  profile(portafolio.profile, portafolio.technologies);
-  experiences(portafolio.experiences);
-  projects(portafolio.projects);
-  skills(portafolio.skills);
-  footer(portafolio.contacts, portafolio.profile);
+
+  const method = 'GET';
+  const url = 'https://elliotxleo.github.io/api-publica/json/portafolio-elliot.json';
+
+  const fetchApi = async () => {
+    try {
+      const options = {
+        method,
+        url
+      };
+      const response = await axios(options);
+      const portafolio = response.data;
+      header();
+      profile(portafolio.profile, portafolio.technologies);
+      experiences(portafolio.experiences);
+      projects(portafolio.projects);
+      skills(portafolio.skills);
+      footer(portafolio.contacts, portafolio.profile);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchApi();
+
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
