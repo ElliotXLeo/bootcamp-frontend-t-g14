ec60950ebed235f3ec31b58a3258be7acf2fcec6
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-13 11:26:15 -0500

    s7.2: Update experiences index

--- semana-7/2-pagina-personal/js/index.js
+++ semana-7/2-pagina-personal/js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 import header from './header.js';
 import profile from './profile.js';
-// import experiencias from './experiencias.js';
+import experiences from './experiences.js';
 // import proyectos from './proyectos.js';
 // import skills from './skills.js';
 // import footer from './footer.js';
@@ -12,7 +12,7 @@ import portafolio from './portafolio-elliot.js';
 const documentReady = () => {
   header();
   profile(portafolio.profile, portafolio.technologies);
-  // experiencia(experiences);
+  experiences(portafolio.experiences);
   // projects(projects);
   // skills(skills);
   // footer(contacts, profile);
