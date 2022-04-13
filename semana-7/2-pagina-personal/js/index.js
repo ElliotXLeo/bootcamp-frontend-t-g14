0b1824f60f10ffe8473023d440fb9af4b7dd6fe1
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-13 12:30:24 -0500

    7.2: Update footer index

--- semana-7/2-pagina-personal/js/index.js
+++ semana-7/2-pagina-personal/js/index.js
@@ -4,7 +4,7 @@ import profile from './profile.js';
 import experiences from './experiences.js';
 import projects from './projects.js';
 import skills from './skills.js';
-// import footer from './footer.js';
+import footer from './footer.js';
 import portafolio from './portafolio-elliot.js';
 
 const documentReady = () => {
@@ -13,7 +13,7 @@ const documentReady = () => {
   experiences(portafolio.experiences);
   projects(portafolio.projects);
   skills(portafolio.skills);
-  // footer(contacts, profile);
+  footer(portafolio.contacts, portafolio.profile);
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
